5c146716462c33947af12d32b038200fa4c7c9b1
gooodwin67 <sn67@inbox.ru>
2022-06-17 19:18:11 +0300

    text

---
+++ lib/menu/text/menu_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextWrap extends StatelessWidget {
+  const TextWrap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
